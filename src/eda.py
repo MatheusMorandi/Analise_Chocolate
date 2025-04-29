95776bc307b3bc92930261e51de1a9fedb1206de
Matheus Morandi <theu.morandi@gmail.com>
2025-04-29 19:24:39 -0300

    feat: primeiro graficos

--- src/eda.py
+++ src/eda.py
@@ -2,9 +2,11 @@
 
 import pandas as pd
 
-import seaborn as sns
+import plotly.express as px
 
-import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # %%
 
@@ -24,7 +26,17 @@ schema = {"Vendedor": str,
 
 ds = pd.read_csv("../data/chocolate_sales.csv", header = 0, names = nomes, dtype = schema)
 
-ds.info()
+print("Resumo do dataset:")
+
+print(ds.info())
+
+print("\nEstatísticas gerais:")
+
+print(ds.describe())
+
+print("\nValores ausentes:")
+
+print(ds.isnull().sum())
 
 # %%
 
@@ -36,7 +48,7 @@ ds["Valor"] = ds["Valor"].str.strip()
 
 ds[["Unit", "Valor($)"]] = ds["Valor"].str.split("$", expand = True)
 
-ds["Valor($)"] = ds["Valor($)"].str.replace(",", "").astype("float")
+ds["Valor($)"] = ds["Valor($)"].str.replace(",", "").astype("int")
 
 ds.drop(["Unit", "Valor"], axis = 1, inplace = True)
 
@@ -45,5 +57,66 @@ ds.drop(["Unit", "Valor"], axis = 1, inplace = True)
 ds["Data"] = pd.to_datetime(ds["Data"])
 
 ds.info()
+
+# %%
+
+ds["Ano"] = ds["Data"].dt.year
+
+ds["Mes"] = ds["Data"].dt.month_name()
+
+# %%
+
+def vlr_total(dados):
+
+    valor_total = dados["Valor($)"].sum()
+
+    valor_total = f"${valor_total:,.2f}"
+
+    return valor_total
+
+# %%
+
+def ticket_medio(dados):
+
+    vnds_totais = len(list(dados.index))
+
+    tck_medio = round(((dados["Valor($)"].sum())/ vnds_totais), 2)
+
+    tck_medio = f"${tck_medio:,.2f}"
+
+    return tck_medio
+
+# %%
+
+def caixas_totais(dados):
+
+    cx_totais = dados["Caixas Enviadas"].sum()
+
+    cx_totais = f"{cx_totais:,}".replace(",", ".")
+
+    return cx_totais
+
+# %%
+
+vnd_pais = ds.groupby("Pais", as_index = False)["Valor($)"].sum().sort_values(by = "Valor($)",ascending = False)
+
+vnd_pais["Valor($)"] = round((vnd_pais["Valor($)"]/1000000), 2)
+
+grfc_pais = px.bar(
+    data_frame = vnd_pais,
+    x = "Valor($)",
+    y = "Pais",
+    color = "Pais",
+    color_discrete_sequence = px.colors.qualitative.Pastel)
+
+grfc_pais.update_layout(
+    title = "Vendas por Países em Milhões ($)",
+    template = "plotly",
+    margin = dict(l = 50, r = 50, b = 50, t = 50),
+    xaxis_title = "Valor das Vendas ($)",
+    yaxis_title = "",
+    showlegend = False)
+
+grfc_pais.show()
 
 # %%
